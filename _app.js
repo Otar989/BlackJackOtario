9789c1d17ed213a2290ce74f03fdce2629b9ae9d
Otar989 <otar989@gmail.com>
2025-07-25 15:14:42 +0300

    Delete _app.js

--- _app.js
+++
@@ -1,6 +0,0 @@
-import '../styles/globals.css';
-
-// Custom App to inject global CSS into the Next.js application
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
